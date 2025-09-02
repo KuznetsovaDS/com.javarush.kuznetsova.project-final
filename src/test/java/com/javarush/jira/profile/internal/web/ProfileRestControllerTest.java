bc1c7333982fe3c1434cf5c4294dc97d29c35af4
Darya Kuznetsova <Dashar1996@gmail.com>
2025-09-02 13:40:26 +0300

    tests for ProfileRestControllerTest done

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,59 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.profile.ProfileTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.javarush.jira.common.util.JsonUtil.writeValue;
+import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.PROFILE_MATCHER;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.USER_PROFILE_TO;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class ProfileRestControllerTest extends AbstractControllerTest {
 
+    private static final String REST_URL = "/api/profile";
+
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void getPositive() throws Exception{
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_MATCHER.contentJson(USER_PROFILE_TO));
+    }
+    @Test
+    void getUnAuth() throws Exception{
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void update() throws Exception{
+
+        ProfileTo updatedTo = ProfileTestData.getUpdatedTo();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_MATCHER.contentJson(updatedTo));
+    }
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void updateInvalid() throws Exception{
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(ProfileTestData.getInvalidTo())))
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
