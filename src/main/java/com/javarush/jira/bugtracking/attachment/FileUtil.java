bc4f84212ae76bb67e8de4dbf84ea91e5fb37033
Darya Kuznetsova <Dashar1996@gmail.com>
2025-09-02 16:43:54 +0300

    method upload in FileUtil with nio

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,14 +7,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 @UtilityClass
 public class FileUtil {
@@ -24,15 +19,16 @@ public class FileUtil {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
-
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
+        Path dirPath = Paths.get(directoryPath);
+        try {
+            if (!Files.exists(dirPath)) {
+                Files.createDirectories(dirPath);
             }
+            Path filePath = dirPath.resolve(fileName);
+            Files.write(filePath, multipartFile.getBytes());
+        }
+        catch (IOException e){
+            throw new IllegalRequestDataException("Failed to upload file " + multipartFile.getOriginalFilename());
         }
     }
 
